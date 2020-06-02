6dcd5bbf1fb6bf5d0b740aaaba5784c0da0ae877
JoseySurfaceBook <darkswordss>
2020-06-01 21:36:29 -0400

    Parses the HTML in findofinder.com

--- Webscraping/fidofinder.py
+++ Webscraping/fidofinder.py
@@ -1,1 +1,21 @@
 from bs4 import BeautifulSoup
+from urllib import  urlopen
+
+#sets url to desired webpage
+url="https://www.fidofinder.com"
+
+#downloads webpage
+Client=urlopen(url)
+
+html=Client.read()
+
+#closes the Webclient
+Client.close()
+
+#uses bs4 to parse the HTML
+page_soup =BeautifulSoup(html).prettify()
+
+print (page_soup)
+
+
+
