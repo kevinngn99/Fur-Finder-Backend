09e155b7d569d768ad1f51ddece1eca94a38ef68
kevinngn99 <kevinnguyen1@ufl.edu>
2020-06-03 22:45:23 -0400

    Data Scrap Hot Fixes

--- lostmydoggie_scrap.py
+++ lostmydoggie_scrap.py
@@ -13,47 +13,47 @@ def get_url(n, p, z):
     url = begin + str(num) + mid1 + str(page) + mid2 + zipcode + end
     return url
 
-zipcode = input('Enter zip code: ')
-url = get_url(1, 1, zipcode)
-soup = BeautifulSoup(requests.get(url).text, 'lxml')
+if __name__ == "__main__":
+    zipcode = input('Enter zip code: ')
+    url = get_url(1, 1, zipcode)
+    soup = BeautifulSoup(requests.get(url).text, 'lxml')
 
-header = soup.find('div', {'class': 'col-lg-12 bg-darkcolor'}).font.text
-count = int(str(header).split(' Lost')[0].split('Showing ')[1])
-print('Number of lost pets: ', count)
-print()
+    header = soup.find('div', {'class': 'col-lg-12 bg-darkcolor'}).font.text
+    count = int(str(header).split(' Lost')[0].split('Showing ')[1])
+    print('Number of lost pets: ', count)
+    print()
 
-num = 1
-page = 1
+    num = 1
+    page = 1
 
-while num < count:
-    soup = BeautifulSoup(requests.get(get_url(num, page, zipcode)).text, 'lxml')
+    while num <= count:
 
-    for column in soup.find_all('div', {'class': 'col-md-3'}):
-        num += 1
+        for column in soup.find_all('div', {'class': 'col-md-3'}):
+            num += 1
 
-        image = 'https://www.lostmydoggie.com/' + column.find('div', {'class': 'image'}).a.img['src']
-        info = column.find('div', {'class': 'info'})
-        detail = column.find('ul', {'class: ', 'custom no-margin'})
+            image = 'https://www.lostmydoggie.com/' + column.find('div', {'class': 'image'}).a.img['src']
+            info = column.find('div', {'class': 'info'})
+            detail = column.find('ul', {'class: ', 'custom no-margin'})
 
-        name = info.h4.text
-        status = info.h6.span.font.text
-        gender = info.h6.text.split(status)[1]
-        location = info.h6.find_next_sibling('h6').find(text=True)
-        zc = info.h6.find_next_sibling('h6').text.split(location)[1]
-        breed = detail.li.text.split('\n')[0]
-        color = detail.li.find_next_sibling('li').text
-        date = detail.li.find_next_sibling('li').find_next_sibling('li').text
+            name = info.h4.text
+            status = info.h6.span.font.text
+            gender = info.h6.text.split(status)[1].encode('ascii',errors='ignore').decode()[1:]
+            location = info.h6.find_next_sibling('h6').find(text=True)
+            zc = info.h6.find_next_sibling('h6').text.split(location)[1]
+            breed = detail.li.text.split('\n')[0]
+            color = detail.li.find_next_sibling('li').text
+            date = detail.li.find_next_sibling('li').find_next_sibling('li').text
 
-        print('----------------')
-        print('Image: ', image)
-        print('Name: ', name)
-        print('Status:', status)
-        print('Gender: ', gender)
-        print('Location: ', location)
-        print('Zip: ', zc)
-        print('Breed: ', breed)
-        print('Color: ', color)
-        print(date)
-
-    page += 1
-    
+            print('----------------')
+            print('Image: ', image)
+            print('Name: ', name)
+            print('Status:', status)
+            print('Gender: ', gender)
+            print('Location: ', location)
+            print('Zip: ', zc)
+            print('Breed: ', breed)
+            print('Color: ', color)
+            print(date)
+            
+        page += 1
+        soup = BeautifulSoup(requests.get(get_url(num, page, zipcode)).text, 'lxml')
