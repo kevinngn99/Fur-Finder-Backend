d68c46f36aad6798a4399a77ca941fcf2e7b635a
darkswordss <joseg047@ufl.edu>
2020-07-13 00:37:03 -0400

    downloads the image from the backend

--- Webscraping/Verification.py
+++ Webscraping/Verification.py
@@ -3,6 +3,27 @@ import json
 import base64
 from prediction import reportPrediction
 
+#to do
+#check for profanity in names, breed, location,
+#check api/pets if pet has been reported as found
+#remove pets that have been reported as found from /api/pets
+#update the pet model to have a found boolean
+
+#maybe check for valid zipcodes?
+
+def isInBackend(name):
+
+    data = requests.get(url='http://192.168.2.14:8000/api/pets//')
+    data = data.json()
+    for index in range(len(data)):
+        inBackend = False
+        if data[index]["name"] == name:
+            inBackend = True
+            print("Pet name is already in database.")
+    return inBackend
+
+
+
 def getImage(image_data):
     #https://stackoverflow.com/questions/54660800/python-convert-image-to-json
     image_data = base64.b64decode(image_data)
@@ -11,14 +32,20 @@ def getImage(image_data):
         file.write(image_data)
 
 def main():
-    data = requests.get(url='http://10.2.0.251:8000/api/reportedpets//')
+    #gets the data from the unverified pet database
+    data = requests.get(url='http://192.168.2.14:8000/api/reportedpets//')
     data = data.json()
+
 
     for i in range(len(data)):
         getImage( data[i]['image'] )
         predict = reportPrediction()
-        if predict == 'is a pet':
-            requests.post(url='http://10.2.0.251:8000/api/pets//', data=data[i])
+        #checks to see if the image is a cat/dog and checks for Duplicates before posting
+        if predict == 'is a pet' and isInBackend(data[i]["name"])==False:
+            requests.post(url='http://192.168.2.14:8000/api/pets//', data=data[i])
+        else: print("not a pet!")
 
 if __name__ == '__main__':
     main()
+
+
