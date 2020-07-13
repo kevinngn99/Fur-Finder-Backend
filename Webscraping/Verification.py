513513a006dd650e229ff1ce18bd74f04737d791
JasonSchramm <jasonschramm@ufl.edu>
2020-07-13 14:49:22 -0400

    profanity check

--- Webscraping/Verification.py
+++ Webscraping/Verification.py
@@ -3,19 +3,19 @@ import json
 import base64
 from prediction import reportPrediction
 
+# pip install profanity 
+from profanity import profanity 
 
 #to do
-#check for profanity in names, breed, location,
 #check api/pets if pet has been reported as found
 #remove pets that have been reported as found from /api/pets
 #update the pet model to have a found boolean
 #we probably need to give each pet a unique identifier
-
 #maybe check for valid zipcodes?
 
 #checks if a pet with a certain name exisits in database
 def isInBackend(id):
-    data = requests.get(url='http://192.168.2.14:8000/api/pets//')
+    data = requests.get(url='http://10.2.0.251:8000/api/pets//')
     data = data.json()
     for index in range(len(data)):
         inBackend = False
@@ -29,6 +29,15 @@ def isLost(id):
     obj = Pet.objects.get(id)
     return(obj.islost)
 
+#check for profanity in names, breed, location,
+def profanityCheck(data):
+    if profanity.contains_profanity( data['name']) == True:
+        return True
+    if profanity.contains_profanity(data['breed']) == True:
+        return True
+    if profanity.contains_profanity(data['location']) == True:
+        return True
+    return False
 
 def getImage(image_data):
     #https://stackoverflow.com/questions/54660800/python-convert-image-to-json
@@ -38,8 +47,9 @@ def getImage(image_data):
         file.write(image_data)
 
 def main():
+    
     #gets the data from the unverified pet database
-    data = requests.get(url='http://192.168.2.14:8000/api/reportedpets//')
+    data = requests.get(url='http://10.2.0.251:8000/api/reportedpets//')
     data = data.json()
 
 
@@ -47,8 +57,8 @@ def main():
         getImage( data[i]['image'] )
         predict = reportPrediction()
         #checks to see if the image is a cat/dog and checks for Duplicates before posting
-        if predict == 'is a pet' and isInBackend(data[i]["id"])==False:
-            requests.post(url='http://192.168.2.14:8000/api/pets//', data=data[i])
+        if predict == 'is a pet' and isInBackend(data[i]["id"])==False and profanityCheck(data[i]) == False:
+            requests.post(url='http://10.2.0.251:8000/api/pets//', data=data[i])
         else: print(data[i]["name"],"not a pet!")
 
 if __name__ == '__main__':
