6e9bb6400043eb93c11fa311828603cced9b738d
darkswordss <joseg047@ufl.edu>
2020-07-15 23:49:55 -0400

    now successfully removes pet listings with Profanity, and those marked as found. Image verification needs further testing

--- Webscraping/Verification.py
+++ Webscraping/Verification.py
@@ -1,50 +1,39 @@
-import requests
-import json
-import base64
-from prediction import reportPrediction
-from prediction import getImageFromInternet
 
-# pip install profanity
+from prediction import reportPrediction
+from prediction import load_image
 from profanity import profanity
 import sqlite3
 
+# checks if a pet with that id exists in database
+# def isInBackend(id):
+#     conn = sqlite3.connect('../db.sqlite3')
+#     c = conn.cursor()
+#     inBackend = c.execute("SELECT EXISTS(SELECT 1 FROM FurFinderAPI_pet WHERE id is 1);").fetchall()
+#     print("inBackend: ",True if inBackend[0][0] == 1 else False)
+#     return True if inBackend[0][0] == 1 else False
+#     conn.close()
 
-# to do
-# remove pets that have been reported as found from /api/pets
-# maybe check for valid zipcodes?
 
-# checks if a pet with a certain name exisits in database
-def isInBackend(id):
+# # given a petID will return if pet is still reported as lost
+# def isLost(id):
+#     # check if pet with Id exisits
+#     if isInBackend(id) == True:
+#         conn = sqlite3.connect('../db.sqlite3')
+#         c = conn.cursor()
+#         lost = c.execute("SELECT islost FROM FurFinderAPI_pet WHERE petid is " + str(id)).fetchall()
+#         return True if lost[0][0] == 1 else False
+#         conn.close()
+#     else:
+#         return "Error pet not in database"
+
+
+#given a pet id remove that Pet
+def removePet(id):
     conn = sqlite3.connect('../db.sqlite3')
     c = conn.cursor()
-    inBackend = c.execute("SELECT EXISTS(SELECT 1 FROM FurFinderAPI_pet WHERE id is 1);").fetchall()
-    print("inBackend: ",True if inBackend[0][0] == 1 else False)
-    return True if inBackend[0][0] == 1 else False
+    c.execute("DELETE FROM FurFinderAPI_pet WHERE petid =='"+ str(id) +"';")
+    conn.commit()
     conn.close()
-
-
-# given a petID will return if pet is still reported as lost
-def isLost(id):
-    # check if pet with Id exisits
-    if isInBackend(id) == True:
-        conn = sqlite3.connect('../db.sqlite3')
-        c = conn.cursor()
-        lost = c.execute("SELECT islost FROM FurFinderAPI_pet WHERE petid is " + str(id)).fetchall()
-        return True if lost[0][0] == 1 else False
-        conn.close()
-    else:
-        return "Error pet not in database"
-
-
-# check for profanity in names, breed, location,
-def profanityCheck(data):
-    if profanity.contains_profanity(data['name']) == True:
-        return True
-    if profanity.contains_profanity(data['breed']) == True:
-        return True
-    if profanity.contains_profanity(data['location']) == True:
-        return True
-    return False
 
 
 # will remove all pets marked as found from api/pets and possibly in the future put it in another database to be
@@ -53,25 +42,41 @@ def removeFoundPets():
 
     conn = sqlite3.connect('../db.sqlite3')
     c = conn.cursor()
-    c.execute("DELETE FROM FurFinderAPI_pet WHERE status IS false;")
+    c.execute("DELETE FROM FurFinderAPI_pet WHERE status =='Found';")
     conn.commit()
     conn.close()
 
+#uses tensorflow to check if image is approved if not then removes it from database
+def removeNonPetImage():
+    conn = sqlite3.connect('../db.sqlite3')
+    c = conn.cursor()
+    imageListing=c.execute("SELECT image,petid FROM FurFinderAPI_pet ;").fetchall()
+
+    for i in range(len(imageListing)):
+        load_image("../media/"+imageListing[i][0])
+        predict = reportPrediction()
+        if predict == "is not a pet":
+            removePet(imageListing[i][1])
+        else: print("Pet id : " + str(imageListing[i][1])+" not removed because it's a pet")
+    conn.commit()
+    conn.close()
+
+#removes all listings of pets where the breed, name, city,and zip contain profanity
+def removePetsWithProfanity():
+    conn = sqlite3.connect('../db.sqlite3')
+    c = conn.cursor()
+    petListing = c.execute("SELECT breed,name,city,zip,petid FROM FurFinderAPI_pet ").fetchall()
+
+    for i in range(len(petListing)):
+        if profanity.contains_profanity(petListing[i][0]) == True or profanity.contains_profanity(petListing[i][1]) == True or profanity.contains_profanity(petListing[i][2]) == True or profanity.contains_profanity(petListing[i][3]) == True:
+           removePet(petListing[i][4])
+    conn.commit()
+    conn.close()
 
 def main():
-    # gets the data from the unverified pet database
-    data = requests.get(url='http://192.168.2.8:8000/api/pets//')
-    data = data.json()
-    #removeFoundPets()
-    for i in range(len(data)):
-
-        getImageFromInternet(data[i]['image'])
-        predict = reportPrediction()
-        print(predict)
-        # checks to see if the image is a cat/dog, checks for Duplicates and profanity before posting
-        if predict == 'is a pet' and isInBackend(data[i]["petid"]) == False and profanityCheck(data[i]) == False:
-            requests.post(url='http://192.168.2.8:8000/api/pets//', data=data[i])
-
+    removeFoundPets()
+    removeNonPetImage()
+    removePetsWithProfanity()
 
 
 if __name__ == '__main__':
