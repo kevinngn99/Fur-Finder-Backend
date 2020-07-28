03ba2ff9826d226f778ae6b52989089a11ac2ad9
Kevin Nguyen <kevinnguyen1@ufl.edu>
2020-07-27 23:30:32 -0400

    Revert Before Messaging

--- FurFinderSITE/settings.py
+++ FurFinderSITE/settings.py
@@ -92,7 +92,8 @@ ASGI_APPLICATION = "FurFinderSITE.routing.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'CONN_MAX_AGE': 500
     }
 }
 
